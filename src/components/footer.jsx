1daf3218aeacf0302377510673655cf0c4398270
CTEREOK <vladimir@yandex.ru>
2023-06-09 13:59:14 +0300

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="page-footer teal lighten-2">
+            <div className="footer-copyright">
+                <div className="container grey-text text-lighten-5">Ⓒ {new Date().getFullYear()} CTEREOK<a className="grey-text text-lighten-5 right" href="!#">Reprository</a></div>
+            </div>
+        </footer>
+    )
+}
+
+export { Footer };
